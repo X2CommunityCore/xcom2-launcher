5e607bfc699d59609a2bfdeba782f45589dddf07
BTernaryTau <alexgarcia99@gmail.com>
2017-08-27 23:20:47 -0400

    Edit auto-number settings text and tooltip

--- xcom2-launcher/xcom2-launcher/Forms/SettingsDialog.Designer.cs
+++ xcom2-launcher/xcom2-launcher/Forms/SettingsDialog.Designer.cs
@@ -28,175 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.components = new System.ComponentModel.Container();
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-			this.label1 = new System.Windows.Forms.Label();
-			this.modPathsListbox = new System.Windows.Forms.ListBox();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.gamePathTextBox = new System.Windows.Forms.TextBox();
-			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-			this.addModPathButton = new System.Windows.Forms.Button();
-			this.removeModPathButton = new System.Windows.Forms.Button();
-			this.browseGamePathButton = new System.Windows.Forms.Button();
-			this.argumentsTextBox = new XCOM2Launcher.UserElements.AutoCompleteTextBox();
-			this.groupBox2 = new System.Windows.Forms.GroupBox();
-			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-			this.searchForUpdatesCheckBox = new System.Windows.Forms.CheckBox();
-			this.showHiddenEntriesCheckBox = new System.Windows.Forms.CheckBox();
-			this.label6 = new System.Windows.Forms.Label();
-			this.closeAfterLaunchCheckBox = new System.Windows.Forms.CheckBox();
-			this.label5 = new System.Windows.Forms.Label();
-			this.categoriesListBox = new System.Windows.Forms.ListBox();
-			this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
-			this.moveCategoryUpButton = new System.Windows.Forms.Button();
-			this.moveCategoryDownButton = new System.Windows.Forms.Button();
-			this.renameCategoryButton = new System.Windows.Forms.Button();
-			this.removeCategoryButton = new System.Windows.Forms.Button();
-			this.autoNumberModIndexesCheckBox = new System.Windows.Forms.CheckBox();
-			this.toolTip = new System.Windows.Forms.ToolTip(this.components);
-			this.groupBox1.SuspendLayout();
-			this.tableLayoutPanel2.SuspendLayout();
-			this.flowLayoutPanel1.SuspendLayout();
-			this.groupBox2.SuspendLayout();
-			this.tableLayoutPanel3.SuspendLayout();
-			this.flowLayoutPanel2.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.modPathsListbox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gamePathTextBox = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.addModPathButton = new System.Windows.Forms.Button();
+            this.removeModPathButton = new System.Windows.Forms.Button();
+            this.browseGamePathButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.groupBox1.Controls.Add(this.tableLayoutPanel2);
-			this.groupBox1.Location = new System.Drawing.Point(12, 8);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(655, 176);
-			this.groupBox1.TabIndex = 9;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "Game";
-			// 
-			// tableLayoutPanel2
-			// 
-			this.tableLayoutPanel2.ColumnCount = 3;
-			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
-			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-			this.tableLayoutPanel2.Controls.Add(this.label1, 0, 2);
-			this.tableLayoutPanel2.Controls.Add(this.modPathsListbox, 1, 1);
-			this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
-			this.tableLayoutPanel2.Controls.Add(this.label4, 0, 1);
-			this.tableLayoutPanel2.Controls.Add(this.gamePathTextBox, 1, 0);
-			this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel1, 2, 1);
-			this.tableLayoutPanel2.Controls.Add(this.browseGamePathButton, 2, 0);
-			this.tableLayoutPanel2.Controls.Add(this.argumentsTextBox, 1, 2);
-			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 16);
-			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-			this.tableLayoutPanel2.RowCount = 3;
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(649, 157);
-			this.tableLayoutPanel2.TabIndex = 6;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(3, 126);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(57, 13);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Arguments";
-			// 
-			// modPathsListbox
-			// 
-			this.modPathsListbox.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.modPathsListbox.FormattingEnabled = true;
-			this.modPathsListbox.Location = new System.Drawing.Point(103, 33);
-			this.modPathsListbox.Name = "modPathsListbox";
-			this.modPathsListbox.Size = new System.Drawing.Size(470, 90);
-			this.modPathsListbox.TabIndex = 4;
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(3, 0);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(56, 13);
-			this.label3.TabIndex = 0;
-			this.label3.Text = "Base Path";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(3, 30);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(81, 13);
-			this.label4.TabIndex = 2;
-			this.label4.Text = "Mod Directories";
-			// 
-			// gamePathTextBox
-			// 
-			this.gamePathTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.gamePathTextBox.Location = new System.Drawing.Point(103, 3);
-			this.gamePathTextBox.Name = "gamePathTextBox";
-			this.gamePathTextBox.Size = new System.Drawing.Size(470, 20);
-			this.gamePathTextBox.TabIndex = 10;
-			// 
-			// flowLayoutPanel1
-			// 
-			this.flowLayoutPanel1.Controls.Add(this.addModPathButton);
-			this.flowLayoutPanel1.Controls.Add(this.removeModPathButton);
-			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.flowLayoutPanel1.Location = new System.Drawing.Point(576, 30);
-			this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
-			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(73, 96);
-			this.flowLayoutPanel1.TabIndex = 12;
-			// 
-			// addModPathButton
-			// 
-			this.addModPathButton.Location = new System.Drawing.Point(3, 3);
-			this.addModPathButton.Name = "addModPathButton";
-			this.addModPathButton.Size = new System.Drawing.Size(67, 24);
-			this.addModPathButton.TabIndex = 6;
-			this.addModPathButton.Text = "Add";
-			this.addModPathButton.UseVisualStyleBackColor = true;
-			this.addModPathButton.Click += new System.EventHandler(this.AddModPathButtonOnClick);
-			// 
-			// removeModPathButton
-			// 
-			this.removeModPathButton.Location = new System.Drawing.Point(3, 33);
-			this.removeModPathButton.Name = "removeModPathButton";
-			this.removeModPathButton.Size = new System.Drawing.Size(67, 24);
-			this.removeModPathButton.TabIndex = 8;
-			this.removeModPathButton.Text = "Remove";
-			this.removeModPathButton.UseVisualStyleBackColor = true;
-			this.removeModPathButton.Click += new System.EventHandler(this.RemoveModPathButtonOnClick);
-			// 
-			// browseGamePathButton
-			// 
-			this.browseGamePathButton.Location = new System.Drawing.Point(579, 3);
-			this.browseGamePathButton.Name = "browseGamePathButton";
-			this.browseGamePathButton.Size = new System.Drawing.Size(67, 24);
-			this.browseGamePathButton.TabIndex = 14;
-			this.browseGamePathButton.Text = "Browse";
-			this.browseGamePathButton.UseVisualStyleBackColor = true;
-			this.browseGamePathButton.Click += new System.EventHandler(this.BrowseGamePathButtonOnClick);
-			// 
-			// argumentsTextBox
-			// 
-			this.argumentsTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.argumentsTextBox.Location = new System.Drawing.Point(103, 129);
-			this.argumentsTextBox.Name = "argumentsTextBox";
-			this.argumentsTextBox.Size = new System.Drawing.Size(470, 20);
-			this.argumentsTextBox.TabIndex = 15;
-			this.argumentsTextBox.Values = new string[0];
-			// 
-			// groupBox2
-			// 
-			this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(655, 176);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Game";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.modPathsListbox, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.gamePathTextBox, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel1, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.browseGamePathButton, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.argumentsTextBox, 1, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(649, 157);
+            this.tableLayoutPanel2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arguments";
+            // 
+            // modPathsListbox
+            // 
+            this.modPathsListbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.modPathsListbox.FormattingEnabled = true;
+            this.modPathsListbox.Location = new System.Drawing.Point(103, 33);
+            this.modPathsListbox.Name = "modPathsListbox";
+            this.modPathsListbox.Size = new System.Drawing.Size(470, 90);
+            this.modPathsListbox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Base Path";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Mod Directories";
+            // 
+            // gamePathTextBox
+            // 
+            this.gamePathTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gamePathTextBox.Location = new System.Drawing.Point(103, 3);
+            this.gamePathTextBox.Name = "gamePathTextBox";
+            this.gamePathTextBox.Size = new System.Drawing.Size(470, 20);
+            this.gamePathTextBox.TabIndex = 10;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.addModPathButton);
+            this.flowLayoutPanel1.Controls.Add(this.removeModPathButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(576, 30);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(73, 96);
+            this.flowLayoutPanel1.TabIndex = 12;
+            // 
+            // addModPathButton
+            // 
+            this.addModPathButton.Location = new System.Drawing.Point(3, 3);
+            this.addModPathButton.Name = "addModPathButton";
+            this.addModPathButton.Size = new System.Drawing.Size(67, 24);
+            this.addModPathButton.TabIndex = 6;
+            this.addModPathButton.Text = "Add";
+            this.addModPathButton.UseVisualStyleBackColor = true;
+            this.addModPathButton.Click += new System.EventHandler(this.AddModPathButtonOnClick);
+            // 
+            // removeModPathButton
+            // 
+            this.removeModPathButton.Location = new System.Drawing.Point(3, 33);
+            this.removeModPathButton.Name = "removeModPathButton";
+            this.removeModPathButton.Size = new System.Drawing.Size(67, 24);
+            this.removeModPathButton.TabIndex = 8;
+            this.removeModPathButton.Text = "Remove";
+            this.removeModPathButton.UseVisualStyleBackColor = true;
+            this.removeModPathButton.Click += new System.EventHandler(this.RemoveModPathButtonOnClick);
+            // 
+            // browseGamePathButton
+            // 
+            this.browseGamePathButton.Location = new System.Drawing.Point(579, 3);
+            this.browseGamePathButton.Name = "browseGamePathButton";
+            this.browseGamePathButton.Size = new System.Drawing.Size(67, 24);
+            this.browseGamePathButton.TabIndex = 14;
+            this.browseGamePathButton.Text = "Browse";
+            this.browseGamePathButton.UseVisualStyleBackColor = true;
+            this.browseGamePathButton.Click += new System.EventHandler(this.BrowseGamePathButtonOnClick);
+            // 
+            // 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.groupBox2.Controls.Add(this.tableLayoutPanel3);
@@ -286,6 +259,11 @@
 			// categoriesListBox
 			// 
 			this.categoriesListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            // 
+            // 
+            // 
+            // 
+            // 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.tableLayoutPanel3.SetColumnSpan(this.categoriesListBox, 2);
@@ -382,6 +360,31 @@
 			this.tableLayoutPanel3.PerformLayout();
 			this.flowLayoutPanel2.ResumeLayout(false);
 			this.ResumeLayout(false);
+            // 
+            // 
+            // SettingsDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 470);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = global::XCOM2Launcher.Properties.Resources.xcom;
+            this.Name = "SettingsDialog";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Settings";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SettingsDialog_FormClosing);
+            this.Shown += new System.EventHandler(this.SettingsDialog_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
 
         }
 
